6896da959b6415b3b65d333bc63f40e1de4e797a
PMclaug1 <pmclaug1@gmail.com>
2023-03-22 17:14:04 -0500

    update

--- week1/rock_paper_scissors.py
+++ week1/rock_paper_scissors.py
@@ -27,16 +27,28 @@ scissors = '''
 ---.__(___)
 '''
 
+game_images = [rock, paper, scissors]
+
 choice_input = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors. "))
 
-computer_play = random.randint(0, 2)
-#print(computer_play)
+if choice_input < 3 and choice_input > 0:
+    print(game_images[choice_input])
 
-if choice_input == 0 and computer_play == 2:
+computer_play = random.randint(0, 2)
+
+if choice_input < 3 and choice_input > 0:
+    print(f"Computer chose {computer_play} {game_images[computer_play]}")
+
+
+if choice_input >= 3 or choice_input < 0:
+    print("You selected an invalid number.")
+elif choice_input == 0 and computer_play == 2:
     print("You win!")
+elif computer_play == 0 and choice_input == 2:
+    print("You lose!")
 elif computer_play > choice_input:
+    print("You lose!")
+elif choice_input > computer_play:
     print("You lose!")
 elif computer_play == choice_input:
     print("You tie!")
-else:
-    print("You win!")
