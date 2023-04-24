9ac766208d823369049ca8873b02675a001372b7
PMclaug1 <pmclaug1@gmail.com>
2023-04-24 17:41:12 -0500

    update

--- weather_app/client/src/views/Homepage.jsx
+++ weather_app/client/src/views/Homepage.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import ButtonAppBar from '../components/NavBar'
+import { Container } from '@mui/material'
+import ResponsiveAppBar from '../components/SearchBar'
+import SearchAppBar from '../components/CitySearch'
+import BasicCard from '../components/TodaysWeather'
+
+
 
 const Homepage = () => {
     const [searchValue, setSearchValue] = useState("")
@@ -8,15 +15,16 @@ const Homepage = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        console.log(searchValue)
         axios.get(`http://api.weatherapi.com/v1/current.json?key=72733a4a54444b9d88c211620232903 &q=${searchValue}&aqi=no`)
-        .then(res => {
-            setWeatherData(res.data.current)
-            setLocationData(res.data.location)
-            console.log(weatherData)
-        })
-        .catch(err=>{
-            console.log("Error for API call", err)
-        })    
+            .then(res => {
+                setWeatherData(res.data.current)
+                setLocationData(res.data.location)
+                console.log(weatherData)
+            })
+            .catch(err => {
+                console.log("Error for API call", err)
+            })
     }
 
     return (
@@ -37,9 +45,20 @@ const Homepage = () => {
                 <h3>{weatherData.feelslike_f}</h3>
 
             </div>
+            <ButtonAppBar />
+            <form onSubmit={handleSubmit}>
+                <SearchAppBar >
+                    {/* <input type="text" onChange={(e) => { setSearchValue(e.target.value) }} /> */}
+                </SearchAppBar>
+            </form>
+            <ResponsiveAppBar />
+            <Container sx={{ marginY: 15 }}>
+                <BasicCard />
+            </Container>
         </div>
     )
 }
 
 export default Homepage
+export const weatherData = weatherData
 
