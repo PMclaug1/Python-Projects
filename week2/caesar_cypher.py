eaedb4735ce1b98dbe03ca29dcf05d1a979331bf
PMclaug1 <pmclaug1@gmail.com>
2023-04-30 14:16:59 -0500

    caesar_cypher

--- week2/caesar_cypher.py
+++ week2/caesar_cypher.py
@@ -4,7 +4,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
 def encrypt(plain_text,shift_amt):
     encrypted_text = ""
     for i in plain_text:
@@ -14,15 +14,19 @@ def encrypt(plain_text,shift_amt):
         encrypted_text += alphabet[new_letter]
     print(f"The encoded text is {encrypted_text}")
 
-encrypt(plain_text = text,shift_amt = shift)
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+def decrypt(encrypted_text, shift_amt):
+    decrypted_text = ""
+    for i in encrypted_text:
+        new_letter = alphabet.index(i) - shift_amt
+        if new_letter < 0:
+            new_letter = 26 + new_letter
+        decrypted_text += alphabet[new_letter]
+    print(f"The encoded text is {decrypted_text}")
+
+if direction == "encode":
+    encrypt(plain_text = text,shift_amt = shift)
+else:
+    decrypt(encrypted_text = text,shift_amt = shift)
 
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
