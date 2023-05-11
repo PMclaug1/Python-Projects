ac3ccb86f867fb73df5e10fc6796697864dffde9
PMclaug1 <pmclaug1@gmail.com>
2023-05-10 22:30:58 -0500

    final calc

--- week2/calculator.py
+++ week2/calculator.py
@@ -25,3 +25,16 @@ calc_functions = {
     "*": multiply,
     "/": divide
 }
+
+num1 = int(input("What is the first number? "))
+num2 = int(input("What is the second number? "))
+
+for operation in calc_functions:
+    print(operation)
+
+operation_symbol = input("Pick an operation from the line above.")
+
+calc_funct = calc_functions[operation_symbol]
+
+answer = calc_funct(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
