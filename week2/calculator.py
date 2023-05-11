dba2072cd1524e9adbb3c749a122263afc0ad27e
PMclaug1 <pmclaug1@gmail.com>
2023-05-10 22:15:48 -0500

    calc1

---
+++ week2/calculator.py
@@ -0,0 +1,27 @@
+import art3
+
+#calculator
+
+#Add
+def add(n1,n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1,n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1,n2):
+    return n1 * n2
+
+#Divide
+def divide(n1,n2):
+    return n1/n2
+
+#calc functions dictionary
+calc_functions = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
