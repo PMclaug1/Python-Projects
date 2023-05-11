9fbdaf0b4a59d745e264ea455e8996fef9a3f2e9
PMclaug1 <pmclaug1@gmail.com>
2023-05-11 00:44:49 -0500

    calculator

--- week2/calculator.py
+++ week2/calculator.py
@@ -1,40 +1,64 @@
 import art3
 
-#calculator
+# calculator
 
-#Add
-def add(n1,n2):
+# Add
+def add(n1, n2):
     return n1 + n2
 
-#Subtract
-def subtract(n1,n2):
+# Subtract
+def subtract(n1, n2):
     return n1 - n2
 
-#Multiply
-def multiply(n1,n2):
+# Multiply
+def multiply(n1, n2):
     return n1 * n2
 
-#Divide
-def divide(n1,n2):
+# Divide
+def divide(n1, n2):
     return n1/n2
 
-#calc functions dictionary
+# Exponent
+def exponent(n1,n2):
+    return n1**n2
+
+
+# calc functions dictionary
 calc_functions = {
     "+": add,
     "-": subtract,
     "*": multiply,
-    "/": divide
+    "/": divide,
+    "^": exponent
 }
 
-num1 = int(input("What is the first number? "))
-num2 = int(input("What is the second number? "))
 
-for operation in calc_functions:
-    print(operation)
+def calculator():
+    print(art3.logo)
+    num1 = float(input("What is the first number? "))
 
-operation_symbol = input("Pick an operation from the line above.")
+    for operation in calc_functions:
+        print(operation)
 
-calc_funct = calc_functions[operation_symbol]
+    continuation = True
 
-answer = calc_funct(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    while continuation:
+        operation_symbol = input("Pick an operation. ")
+        calc_funct = calc_functions[operation_symbol]
+        num2 = float(input("What is the next number? "))
+        answer = calc_funct(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        should_continue = input(
+            f"Type 'y' to continue calculating with {answer}, or 'n' to start a new calculation. Type 'exit' to quit. ").lower()
+        if should_continue == "y":
+            continuation = True
+            num1 = answer
+        elif should_continue == "n":
+            continuation = False
+            calculator()
+        else:
+            break
+            
+
+
+calculator()
