5a0ed6da197dbc18b89ef2a6b7aea7204f448f2c
PMclaug1 <pmclaug1@gmail.com>
2023-05-20 16:42:01 -0500

    refactor

--- week3/higher_lower.py
+++ week3/higher_lower.py
@@ -16,6 +16,8 @@ def play_game():
     player_score = 0
     a_comparison = random.choice(data)
     b_comparison = random.choice(data)
+    if a_comparison == b_comparison:
+        b_comparison = random.choice
     should_continue = True
 
     while should_continue:
@@ -25,6 +27,7 @@ def play_game():
         choice = input("Who has more followers? Type 'A' or 'B'? ").upper()
     
         if choice == 'B':
+            print(f"{b_comparison['follower_count']} > {a_comparison['follower_count']}")
             if b_comparison['follower_count'] > a_comparison['follower_count']:
                 player_score += 1
                 print(f"You're right! Current score: {player_score}.")
@@ -32,6 +35,7 @@ def play_game():
                 print(f"Sorry, that's wrong. Final score: {player_score}")
                 should_continue = False
         else:
+            print(f"{a_comparison['follower_count']} > {b_comparison['follower_count']}")
             if a_comparison['follower_count'] > b_comparison['follower_count']:
                 player_score += 1
                 print(f"You're right! Current score: {player_score}.")
