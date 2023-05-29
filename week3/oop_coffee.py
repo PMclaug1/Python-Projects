716cbe1cfcec62185624092f1bf8e8cb2b7922b0
PMclaug1 <pmclaug1@gmail.com>
2023-05-29 05:23:13 -0500

    ignore

--- week3/oop_coffee.py
+++ week3/oop_coffee.py
@@ -1,9 +1,12 @@
 from turtle import Turtle, Screen
 
+
 timmy = Turtle()
 
 print(timmy)
 timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
