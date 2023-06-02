73dd528c69e3426997f9ca316cb7ad29ecf4b845
PMclaug1 <pmclaug1@gmail.com>
2023-06-02 18:17:48 -0500

    class_info

--- week3/class_info.py
+++ week3/class_info.py
@@ -1,5 +1,15 @@
 #Name classes w/ Pascal WaaaWaaaWaaa
 class User:
-    pass
+    def __init__(self):
+        print("new user being created...")
 
 user_1 = User()
+#attributes - manually added, better to user constructors
+user_1.id = "001"
+user_1.username = "First_Employee"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Bob"
