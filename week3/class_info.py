284ec76380d96c4570d6ea399402004273e3f8ea
PMclaug1 <pmclaug1@gmail.com>
2023-06-04 16:49:48 -0500

    class_info

--- week3/class_info.py
+++ week3/class_info.py
@@ -1,15 +1,33 @@
 #Name classes w/ Pascal WaaaWaaaWaaa
 class User:
-    def __init__(self):
+    #constructor attributes - must include when creating new object. Can provide default value and change when creating.
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
         print("new user being created...")
 
-user_1 = User()
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "First Employee")
+user_1.followers = 5
 #attributes - manually added, better to user constructors
-user_1.id = "001"
-user_1.username = "First_Employee"
+# user_1.id = "001"
+# user_1.username = "First_Employee"
 
 print(user_1.username)
 
-user_2 = User()
-user_2.id = "002"
-user_2.username = "Bob"
+
+user_2 = User("002", "Bob")
+# user_2.id = "002"
+# user_2.username = "Bob"
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.username)
+print(user_2.followers)
