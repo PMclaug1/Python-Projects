3f66e8bf61352a69ac36cee77c21b9c37790f322
PMclaug1 <pmclaug1@gmail.com>
2023-06-17 00:26:08 -0500

    turtle_spirograph

--- week3/turtle_walk.py
+++ week3/turtle_walk.py
@@ -1,13 +1,20 @@
 import turtle as t
 import random
 
+t.colormode(255)
+
 tim = t.Turtle()
 tim.pensize(10)
 tim.speed(10)
 
 ########### Challenge 4 - Random Walk ########
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    rand_color = (r,g,b)
+    return rand_color
 
 def walk():
     for _ in range(10):
@@ -15,5 +22,5 @@ def walk():
         tim.seth(random.randrange(0,360,90))
 
 for _ in range(10):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     walk()
