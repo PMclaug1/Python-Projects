94a3544408b272313aab3069ef685673f0df4a1f
PMclaug1 <pmclaug1@gmail.com>
2023-06-22 10:41:09 -0500

    Pong

---
+++ pythonProject1/main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+
+screen.listen()
+screen.onkey(l_paddle.up, "Up")
+screen.onkey(l_paddle.down, "Down")
+screen.onkey(r_paddle.up, "w")
+screen.onkey(r_paddle.down, "s")
+
+game_on = True
+while game_on:
+    screen.update()
+
+screen.exitonclick()
---
+++ pythonProject1/paddle.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+MOVE_DIST = 20
+UP = 90
+DOWN = 270
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.create_paddle(position)
+
+    def create_paddle(self, position):
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
