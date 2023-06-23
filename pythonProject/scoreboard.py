31cd695822fe45f35adf43076ba6cfe150b7a00a
PMclaug1 <pmclaug1@gmail.com>
2023-06-23 10:31:30 -0500

    snake high score 1

--- pythonProject/scoreboard.py
+++ pythonProject/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -15,13 +16,15 @@ class Scoreboard(Turtle):
         self.refresh_score()
 
     def refresh_score(self):
-        self.write(f"Scoreboard: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Scoreboard: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def add_points(self):
         self.score += 1
-        self.clear()
         self.refresh_score()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game over!", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.refresh_score()
--- pythonProject/snake.py
+++ pythonProject/snake.py
@@ -54,3 +54,10 @@ class Snake:
 
     def eat_food(self):
         self.add_snake(self.segments[-1].position())
+
+    def reset_snake(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
