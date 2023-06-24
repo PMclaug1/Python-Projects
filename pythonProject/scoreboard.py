57ed1e0741b657c04e96398d7ff7e04bb553ea33
PMclaug1 <pmclaug1@gmail.com>
2023-06-23 20:45:17 -0500

    snake high score final

--- pythonProject/scoreboard.py
+++ pythonProject/scoreboard.py
@@ -3,12 +3,15 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 20, "normal")
 
+with open("high_score.txt") as high_score:
+    contents = high_score.read()
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(contents)
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -26,5 +29,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("high_score.txt", mode="w") as high_score:
+                high_score.write(str(self.high_score))
         self.score = 0
         self.refresh_score()
