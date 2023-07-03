872c0f693fbafcc3ba610dadf8a88bc2f1917029
PMclaug1 <pmclaug1@gmail.com>
2023-07-03 16:23:10 -0500

    lists final

--- lists/Lib/main.py
+++ lists/Lib/main.py
@@ -22,4 +22,25 @@ print(short_names)
 long_names = [name.upper() for name in names if len(name) > 5]
 print(long_names)
 
+# square the nubmers list
+numbers = [1,1,2,3,5,8,13,21,34,55]
 
+squared_numbers = [n**2 for n in numbers]
+print(squared_numbers)
+
+# get even numbers in result
+numbers2 = [1,1,2,3,5,8,13,21,34,55]
+result = [n for n in numbers2 if n % 2 == 0]
+print(result)
+
+# comparing lists is separate files
+with open("file1.txt", "r") as file1:
+    file1_data = file1.readlines()
+with open("file2.txt", "r") as file2:
+    file2_data = file2.readlines()
+
+result = [int(n) for n in file1_data if n == n in file2_data]
+
+# Write your code above 👆
+
+print(result)
