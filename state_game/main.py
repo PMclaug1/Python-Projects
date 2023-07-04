b0a9e9afd5c8e919486365fbdc587a76305849e5
PMclaug1 <pmclaug1@gmail.com>
2023-07-03 21:08:27 -0500

    states refactored - list comprehension

--- state_game/main.py
+++ state_game/main.py
@@ -16,10 +16,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state name?").title()
 
     if answer_state == "Exit":
-        missed_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missed_states.append(state)
+        missed_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missed_states)
         new_data.to_csv("states_to_learn.csv")
         break
