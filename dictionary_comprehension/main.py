1cd041e6c7f268743cd743950fea340942e4ac0d
PMclaug1 <pmclaug1@gmail.com>
2023-07-04 22:28:33 -0500

    dictionary comprehension

---
+++ dictionary_comprehension/main.py
@@ -0,0 +1,5 @@
+import  random
+
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+student_scores = {student:random.randint(1,100) for student in names}
+passed_students = {student:score for (student, score) in student_scores.items() if score >= 60}
