a86e6a096a281c9ce072824c8100c1d69603ef67
PMclaug1 <pmclaug1@gmail.com>
2023-07-07 18:04:56 -0500

    dictionary comprehension final

--- dictionary_comprehension/main.py
+++ dictionary_comprehension/main.py
@@ -1,4 +1,5 @@
 import  random
+import pandas
 
 names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
 student_scores = {student:random.randint(1,100) for student in names}
@@ -27,3 +28,25 @@ weather_c = {
 weather_f = {day:((temp * 9/5) + 32) for (day, temp) in weather_c.items()}
 
 print(weather_f)
+
+#looping through dictionaries
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+for (key, value) in student_dict.items():
+    print(value)
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+#Loop through data frame - columns less useful than rows
+for (key,value) in student_data_frame.items():
+    print(value)
+
+#Loop through rows
+for (index, row) in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(row.score)
+
