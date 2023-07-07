a2498bce980ee3507b63e58234e298fcd3f6963a
PMclaug1 <pmclaug1@gmail.com>
2023-07-06 20:13:53 -0500

    dictionary comprehension exercise 2

--- dictionary_comprehension/main.py
+++ dictionary_comprehension/main.py
@@ -10,6 +10,20 @@ print(passed_students)
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 result = {letter:len(letter) for letter in sentence.split()}
 
-
-
 print(result)
+
+# convert temps from c to f
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day:((temp * 9/5) + 32) for (day, temp) in weather_c.items()}
+
+print(weather_f)
