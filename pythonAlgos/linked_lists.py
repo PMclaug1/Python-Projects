30960e22ee1da78c45f8ee7ec6310d1c6783e004
PMclaug1 <pmclaug1@gmail.com>
2023-07-14 07:13:23 -0500

    linked lists - prepend

--- pythonAlgos/linked_lists.py
+++ pythonAlgos/linked_lists.py
@@ -1,10 +1,12 @@
-#constructors
+# constructors
 
 # node class to create nodes
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
 class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
@@ -40,7 +42,7 @@ class LinkedList:
             return None
         temp = self.head
         pre = self.head
-        while(temp.next):
+        while temp.next:
             pre = temp
             temp = temp.next
         self.tail = pre
@@ -51,15 +53,26 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
-my_linked_list = LinkedList(4)
-my_linked_list.append(6)
-my_linked_list.append(8)
-my_linked_list.pop()
+
+my_linked_list = LinkedList(2)
+my_linked_list.append(3)
+my_linked_list.prepend(1)
+
+print('\n\nAfter prepend():')
+print('---------------')
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length, '\n')
+print('Linked List:')
 my_linked_list.print_list()
-
-
-
-
-
-
