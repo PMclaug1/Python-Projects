53ef9e35cd61e7831ec876b4f6ee10d22ee15342
PMclaug1 <pmclaug1@gmail.com>
2023-07-13 20:47:03 -0500

    linked lists 3

--- pythonAlgos/linked_lists.py
+++ pythonAlgos/linked_lists.py
@@ -35,8 +35,27 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+
 my_linked_list = LinkedList(4)
 my_linked_list.append(6)
+my_linked_list.append(8)
+my_linked_list.pop()
 my_linked_list.print_list()
 
 
