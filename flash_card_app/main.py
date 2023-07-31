561ab73b5350ac7594b8e4366359571b3485020a
PMclaug1 <pmclaug1@gmail.com>
2023-07-31 18:55:40 -0500

    flashcard app - create new caards

--- flash_card_app/main.py
+++ flash_card_app/main.py
@@ -1,6 +1,18 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("./data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 window = Tk()
 window.title("Flashcard")
@@ -10,16 +22,18 @@ canvas = Canvas(width=800, height=550)
 card_front = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 275, image=card_front)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 40, "italic"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 40, "italic"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 incorrect_img = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=incorrect_img, highlightthickness=0)
+unknown_button = Button(image=incorrect_img, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 correct_img = PhotoImage(file="./images/right.png")
-known_button = Button(image=correct_img, highlightthickness=0)
+known_button = Button(image=correct_img, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
