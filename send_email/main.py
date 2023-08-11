99b2919587bd557b6c0ecb7c13e049f99cab7482
PMclaug1 <pmclaug1@gmail.com>
2023-08-10 20:37:07 -0500

    datetime

--- send_email/main.py
+++ send_email/main.py
@@ -1,4 +1,5 @@
 import smtplib
+import datetime as dt
 
 my_email = "test_email@gmail.com"
 password = "iqws wmbl nceg fwwy"
@@ -8,3 +9,15 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.login(user=my_email, password=password)
     connection.sendmail(from_addr=my_email, to_addrs="email_to_goes_here", msg="Subject:Test Email\n\nBody of email")
     connection.close()
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+if year == 2023:
+    print("Test")
+
+date_of_birth = dt.datetime(year=1990, month=10, day=1)
+
+
+
