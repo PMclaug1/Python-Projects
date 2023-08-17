9dda6bfb9105d5aeaf5264326d12d5342187803a
PMclaug1 <pmclaug1@gmail.com>
2023-08-16 22:58:03 -0500

    DLL append

--- send_email/doubly_linked_list.py
+++ send_email/doubly_linked_list.py
@@ -1,31 +1,50 @@
-# class Node:
-## WRITE NODE CONSTRUCTOR HERE ##
-#                               #
-#                               #
-#                               #
-#                               #
-#################################
-
-# class DoublyLinkedList:
-## WRITE DLL CONSTRUCTOR HERE ##
-#                              #
-#                              #
-#                              #
-#                              #
-################################
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
 
 
-my_doubly_linked_list = DoublyLinkedList(7)
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    ## WRITE APPEND METHOD HERE ##
+    #                            #
+    #                            #
+    #                            #
+    #                            #
+    ##############################
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
 
 print('Head:', my_doubly_linked_list.head.value)
 print('Tail:', my_doubly_linked_list.tail.value)
-print('Length:', my_doubly_linked_list.length)
+print('Length:', my_doubly_linked_list.length, '\n')
+
+print('Doubly Linked List:')
+my_doubly_linked_list.print_list()
 
 """
     EXPECTED OUTPUT:
     ----------------
-    Head: 7
-    Tail: 7
-    Length: 1
+    Head: 1
+    Tail: 2
+    Length: 2 
+
+    Doubly Linked List:
+    1
+    2
 
 """
