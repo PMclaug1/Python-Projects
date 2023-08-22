44755743a6a81db8e731efaefdb6aad65904f896
PMclaug1 <pmclaug1@gmail.com>
2023-08-21 20:28:54 -0500

    DLL pop

--- send_email/doubly_linked_list.py
+++ send_email/doubly_linked_list.py
@@ -30,26 +30,72 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
 
-my_doubly_linked_list = DoublyLinkedList(1)
-my_doubly_linked_list.append(2)
+    ## WRITE PREPEND METHOD HERE ##
+    #                             #
+    #                             #
+    #                             #
+    #                             #
+    ###############################
 
+
+my_doubly_linked_list = DoublyLinkedList(2)
+my_doubly_linked_list.append(3)
+
+print('Before prepend():')
+print('----------------')
 print('Head:', my_doubly_linked_list.head.value)
 print('Tail:', my_doubly_linked_list.tail.value)
 print('Length:', my_doubly_linked_list.length, '\n')
+print('Doubly Linked List:')
+my_doubly_linked_list.print_list()
 
+my_doubly_linked_list.prepend(1)
+
+print('\n\nAfter prepend():')
+print('---------------')
+print('Head:', my_doubly_linked_list.head.value)
+print('Tail:', my_doubly_linked_list.tail.value)
+print('Length:', my_doubly_linked_list.length, '\n')
 print('Doubly Linked List:')
 my_doubly_linked_list.print_list()
 
 """
     EXPECTED OUTPUT:
+
+    Before prepend():
     ----------------
-    Head: 1
-    Tail: 2
+    Head: 2
+    Tail: 3
     Length: 2 
+
+    Doubly Linked List:
+    2
+    3
+
+
+    After prepend():
+    ---------------
+    Head: 1
+    Tail: 3
+    Length: 3 
 
     Doubly Linked List:
     1
     2
+    3
 
 """
