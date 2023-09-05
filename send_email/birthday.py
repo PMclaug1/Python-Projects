ca9aeba4f19d38543ca0283ade8622ebd520b873
PMclaug1 <pmclaug1@gmail.com>
2023-09-05 17:48:12 -0500

    final

--- send_email/birthday.py
+++ send_email/birthday.py
@@ -1,6 +1,11 @@
 import datetime as dt
+import smtplib
+
 import pandas
 import random
+
+MY_EMAIL = "testemail@gmail.com"
+MY_PASSWORD = "samplepass"
 
 today = (dt.datetime.now().month, dt.datetime.now().day)
 today_tuple = (today.month, today.day)
@@ -14,4 +19,13 @@ if today_tuple in birthdays_dict:
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!:\n\n{contents}"
+        )
