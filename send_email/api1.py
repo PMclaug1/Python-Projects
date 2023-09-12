2ab0db45ba78993d4145f41c8e25785b53f30e7f
PMclaug1 <pmclaug1@gmail.com>
2023-09-12 10:55:28 -0500

    sunrise api

--- send_email/api1.py
+++ send_email/api1.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -10,3 +11,23 @@ latitude = data["iss_position"]["latitude"]
 
 iss_position = (longitude,latitude)
 print(iss_position)
+
+parameters = {
+    "lat": latitude,
+    "long": longitude,
+    "formatted": 0
+}
+
+sunrise_response = requests.get(url=f"https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+
+sunrise_data = sunrise_response.json()
+sunrise = sunrise_data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = sunrise_data["results"]["sunset"].split("T")[1].split(":")[0]
+weather_data = (sunrise, sunset)
+
+print(weather_data)
+
+time_now = datetime.now()
+
+print(time_now.hour)
