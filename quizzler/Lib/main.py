14161fb6504bc268453d7bd60ea0f7482944a53c
PMclaug1 <pmclaug1@gmail.com>
2023-09-21 23:11:42 -0500

    quiz update - tkinter ui 1

--- quizzler/Lib/main.py
+++ quizzler/Lib/main.py
@@ -2,6 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 import requests
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -12,6 +13,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
 while quiz.still_has_questions():
     quiz.next_question()
