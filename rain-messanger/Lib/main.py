704af0ef6d7209dd1b21bdb5fa4121358092bf33
PMclaug1 <pmclaug1@gmail.com>
2023-10-09 23:03:40 -0500

    rain messanger API formatting

--- rain-messanger/Lib/main.py
+++ rain-messanger/Lib/main.py
@@ -1,15 +1,21 @@
 import requests
+import time
+
 
 API_KEY = "537b09cd56c297c0712f4aee4197f6e8"
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
 
 weather_params = {
     "lat": 41.878113,
-    "lon": 87.629799,
-    "appid": API_KEY
+    "lon": -87.629799,
+    "appid": API_KEY,
+    "exclude": "current,minutely,daily",
+    "units": "imperial"
 }
 
 r = requests.get(OWM_ENDPOINT, params=weather_params)
-print(r.status_code)
+r.raise_for_status()
+weather_data = r.json()
+print(weather_data)
 
 
