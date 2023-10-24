0c66f9e4f47795bbbffa4bec045c4eda752d8587
PMclaug1 <pmclaug1@gmail.com>
2023-10-24 17:08:54 -0500

    stock trader final

--- stock_trader/main.py
+++ stock_trader/main.py
@@ -12,9 +12,12 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = "6f3e0fb1db6f4f9bba72300d8f704e4b"
 STOCK_API_KEY = "V7R1I2VX9N761L17"
+ACCOUNT_SID = 'ACe72d733d8d71bfbdf3f874fcb41f563d'
+AUTH_TOKEN = '83b9c91a862fe2f626b38c8619cb6335'
 
 NEWS_PARAMS = {
-
+    "apiKey": NEWS_API_KEY,
+    "qInTitle": COMPANY_NAME
 }
 
 STOCK_PARAMS = {
@@ -32,51 +35,40 @@ r = requests.get(STOCK_ENDPOINT, params=STOCK_PARAMS)
 r.raise_for_status()
 stock_data = r.json()["Time Series (Daily)"]
 formatted_stock_data = [val for (key, val) in stock_data.items()]
-print(formatted_stock_data)
+# print(formatted_stock_data)
 
 yesterday_price = float(formatted_stock_data[0]["4. close"])
-print(yesterday_price)
+# print(yesterday_price)
 day_before_yesterday_price = float(formatted_stock_data[1]["4. close"])
-print(yesterday_price, day_before_yesterday_price)
+# print(yesterday_price, day_before_yesterday_price)
 stock_change = abs(yesterday_price - day_before_yesterday_price)
-print(stock_change)
+# print(stock_change)
 percent_change = stock_change/yesterday_price * 100
-print(percent_change)
+# print(percent_change)
 
 if percent_change > 5:
     print("Get News.")
+    r_news = requests.get(NEWS_ENDPOINT, params=NEWS_PARAMS)
+    r_news.raise_for_status()
+    news_data = r_news.json()["articles"]
+    # print(news_data)
+    formatted_news_data = news_data[:3]
+    print(formatted_news_data)
+    formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in formatted_news_data]
+    # Twilio
+
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+18884877013",
+            to='+16306396125'
+    )
 else:
     print("Minor change.")
 
 
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
 
 
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
-
-#        proxy_client = TwilioHttpClient()
-#        proxy_client.session.proxies = {'https': os.environ['https_proxy']}
-#        client = Client(account_sid, auth_token, http_client=proxy_client)
-
-#        message = client.messages.create(
-#            body="Bring an umbrella.",
-#            from_="+18884877013",
-#            to='+16306396125'
- #       )
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
