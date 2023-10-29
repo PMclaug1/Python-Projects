896afaf671f9f6fd61acfaa39c49edf38973ee0b
PMclaug1 <pmclaug1@gmail.com>
2023-10-28 19:37:46 -0500

    pixela graph creation and post 1

--- habit_tracker/Lib/main.py
+++ habit_tracker/Lib/main.py
@@ -1,10 +1,14 @@
 import requests
 
+USERNAME = "pmclaug1-github"
+TOKEN = "secret-password"
+GRAPH_ID = "graph1"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 pixela_params = {
-    "token": "secret-password",
-    "username": "pmclaug1-github",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
@@ -12,4 +16,38 @@ pixela_params = {
 # pixela set up - completed
 # response = requests.post(url=pixela_endpoint, json=pixela_params)
 # print(response.text)
+
+# pixela set up graph - completed
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+# graph_params = {
+#    "id": "graph1",
+#    "name": "Practice Hours Graph",
+#    "unit": "Hours",
+#    "type": "float",
+#    "color": "ajisai"
+# }
+
+# headers = {
+#    "X-USER-TOKEN": TOKEN
+# }
+
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+
+# create pixela post
+
+graph_entry_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+graph_params = {
+    "date": "20231028",
+    "quantity": "0.5",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_entry_endpoint, json=graph_params, headers=headers)
+print(response.text)
 
